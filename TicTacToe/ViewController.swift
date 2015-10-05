7a699545904ba45c0b6841b8834056ac63d38158
Bob Pascazio <bob@bytefly.com>
2015-10-05 18:01:25 -0400

    updates

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tl: UIImageView!
+    @IBOutlet weak var tm: UIImageView!
+    @IBOutlet weak var tr: UIImageView!
+    @IBOutlet weak var ml: UIImageView!
+    @IBOutlet weak var mm: UIImageView!
+    @IBOutlet weak var mr: UIImageView!
+    @IBOutlet weak var bl: UIImageView!
+    @IBOutlet weak var bm: UIImageView!
+    @IBOutlet weak var br: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tapGestureRecognizer = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
+        
+        // TODO 1: Add the other squares to the gesture recognizer
+        
+        tl.addGestureRecognizer(tapGestureRecognizer)
     }
 
+    func imageTapped(sender: AnyObject)
+    {
+        print("image tapped")
+
+        // TODO 2: Connect the gesture recognizer to the logic class
+        
+        // TODO 3: Change the image according to the logic class with code
+        // similar to this
+        //        let gesture = sender as! UITapGestureRecognizer
+        //        let imageView = gesture.view as! UIImageView
+        //        imageView.image = UIImage(named:"o")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
